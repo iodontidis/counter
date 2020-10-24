a2e2d432a308459a0e59833e273d805a327d2906
Ioannis <idontidis@gmail.com>
2020-10-24 20:46:25 +0200

    UpdateController

--- counter/src/counter/FXMLDocumentController.java
+++ counter/src/counter/FXMLDocumentController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -20,11 +22,15 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private Button btnReset;
+    @FXML
+    private TextField txtCounter;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        System.out.println("Reset pushed");
+        
     }
     
     @Override
